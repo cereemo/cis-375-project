34badc9462bcc262a1597505124c2fddc99b80e5
danielcobb <danielcobb20@gmail.com>
2025-12-01 11:40:37 -0500

    finished the product page, along with the product listings, and updated the slideshows

---
+++ frontend/products.js
@@ -0,0 +1,36 @@
+//product list
+
+const products = [
+    //just for demo 
+    {
+        id: "calculator",
+        name: "Casio Calculator",
+        price: 29.99,
+        image: "img/calculator.jpg",
+        description: "basic Casio calculator."
+    },
+
+    {
+        id: "earbuds",
+        name: "Wired USB-C Earbuds",
+        price: 19.99,
+        image: "img/earbuds.jpg",
+        description: "Wired USB-C earbuds for everyday listening."
+    },
+
+    {
+        id: "hoodie",
+        name: "Gray Hoodie",
+        price: 49.99,
+        image: "img/hoodie.jpg",
+        description: "Gray hoodie to keep you warm in the cold"
+    },
+
+    {
+        id: "laptop",
+        name: "HP Laptop",
+        price: 299.99,
+        image: "img/laptop.jpg",
+        description: "HP laptop for everyday use"
+    },
+]
