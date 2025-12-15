bd285afafa053fa6d61601a8df3a2a9c93c7a1b8
kushalsaraiya1 <kushal.saraiya.1@gmail.com>
2025-12-15 11:57:43 -0500

    Fixed CORS Preflight

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -49,6 +49,19 @@ int main() {
 
     drogon::app().loadConfigFile("./config.json");
 
+    drogon::app().registerPreRoutingAdvice([](const drogon::HttpRequestPtr &req, drogon::FilterCallback &&stop, drogon::FilterChainCallback &&pass) {
+        if (req->method() == drogon::HttpMethod::Options) {
+            auto resp = drogon::HttpResponse::newHttpResponse();
+            resp->addHeader("Access-Control-Allow-Origin", "*");
+            resp->addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+            resp->addHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
+
+            stop(resp);
+            return;
+        }
+        pass();
+    });
+
     drogon::app().registerPostHandlingAdvice([](const drogon::HttpRequestPtr &, const drogon::HttpResponsePtr &resp) {
         resp->addHeader("Access-Control-Allow-Origin", "*");
         resp->addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
